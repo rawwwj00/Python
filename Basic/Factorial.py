7c68226ed0bb3353ee4fb0cee27badb562713d7b
RAJ SACHAN <raj.24bce10022@vitbhopal.ac.in>
2025-01-18 18:52:49 +0530

    Update Factorial.py

--- Basic/Factorial.py
+++ Basic/Factorial.py
@@ -1,4 +1,24 @@
 a=int(input("Enter the number: "))
-for i in range(2,a):
-    a=a*i
-print('Factorial is:',a)
+
+
+*Method 1 (Iteration):
+if a<0:
+    print("The number is negative hence factorial cannot be calculated!")
+elif a<2:
+    print(f'Factorial is: 1')
+else:       
+    for i in range(2,a):
+        a=a*i
+    print(f'Factorial is: {a}')
+
+
+*Method 2 (Recursion):
+def factorial(x):
+    if a<0:
+        return "The number is negative hence factorial cannot be calculated!"
+    elif a<2:
+        return 1
+    else:
+        return x*factorial(x-1)
+fact=factorial(a)
+print(f"The factorial of {a} is {fact}")
